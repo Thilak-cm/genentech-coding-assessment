121ee33817acb19b2a2b5883eb8382e004c8e019
Thilak-cm <thilak.cm212@gmail.com>
2026-01-29 23:50:43 +0000

    minor changes

--- question_2_sdtm/02_create_ds_domain.R
+++ question_2_sdtm/02_create_ds_domain.R
@@ -40,73 +40,76 @@ study_ct <- data.frame(
   )
 )
 
+study_ct <- rbind(
+  study_ct,
+  data.frame(
+    stringsAsFactors = FALSE,
+    codelist_code = "C66727",
+    term_code = NA_character_,
+    term_value = c(
+      "SCREEN FAILURE",
+      "STUDY TERMINATED BY SPONSOR",
+      "LOST TO FOLLOW-UP",
+      "RANDOMIZED"
+    ),
+    collected_value = c(
+      "Screen Failure",
+      "Study Terminated by Sponsor",
+      "Lost to Follow-Up",
+      "Randomized"
+    ),
+    term_preferred_term = NA_character_,
+    term_synonyms = NA_character_
+  )
+)
+
 ds <- ds_raw %>%
   mutate(
     STUDYID = STUDY,
-    DOMAIN  = "DS",
-    USUBJID = paste(STUDY, PATNUM, sep = "-")
-  )
-
-ds <- ds %>%
-  mutate(
-    DSTERM = ifelse(
-      !is.na(OTHERSP),
-      OTHERSP,
-      IT.DSDECOD
+    DOMAIN = "DS",
+    USUBJID = paste(STUDY, PATNUM, sep = "-"),
+    DSCAT = "DISPOSITION EVENT",
+    VISITNUM = INSTANCE,
+    VISIT = as.character(INSTANCE),
+    # Topic: use "Other, specify" when present, else the collected disposition value
+    DSTERM = ifelse(!is.na(OTHERSP), OTHERSP, IT.DSDECOD),
+    # ISO8601 datetime (date + optional time)
+    DSDTC = sdtm.oak::create_iso8601(
+      IT.DSSTDAT,
+      DSTMCOL,
+      .format = list(
+        c("d-m-y"),
+        c("H:M", "H:M:S", "HH:MM", "HH:MM:SS")
+      ),
+      .na = c("UN", "UNK"),
+      .warn = TRUE
     ),
-    DSDECOD = IT.DSDECOD,
-    DSCAT   = "DISPOSITION EVENT"
-  )
-
-# Apply study-specific controlled terminology using {sdtm.oak} structure
-ds <- ds %>%
-  left_join(
-    study_ct,
-    by = c("DSTERM" = "collected_value")
-  ) %>%
-  mutate(
-    DSDECOD = coalesce(term_preferred_term, DSDECOD)
-  )
-
-
-ds <- ds %>%
-  mutate(
-    DS_DATE = as.Date(IT.DSSTDAT, format = "%d-%m-%Y")
-  )
-
-ds <- ds %>%
-  mutate(
-    DSDTC = ifelse(
-      is.na(DSTMCOL),
-      format(DS_DATE, "%Y-%m-%d"),
-      paste0(format(DS_DATE, "%Y-%m-%d"), "T", DSTMCOL)
+    DSSTDTC = DSDTC,
+    # Parsed Date for study day derivation
+    DS_DATE = as.Date(IT.DSSTDAT, format = "%d-%m-%Y"),
+    # Controlled terminology mapping for DSDECOD to standard term_value
+    DSDECOD = sdtm.oak::ct_map(
+      x = IT.DSDECOD,
+      ct_spec = study_ct,
+      ct_clst = "C66727",
+      from = c("collected_value", "term_synonyms", "term_preferred_term"),
+      to = "term_value"
     )
-  )
-
-ds <- ds %>%
-  mutate(
-    DSSTDTC = DSDTC
   )
 
 ref_dates <- ds %>%
   group_by(USUBJID) %>%
   summarise(
-    REF_DATE = if (all(is.na(DS_DATE))) NA else min(DS_DATE, na.rm = TRUE),
+    REF_DATE = if (all(is.na(DS_DATE))) as.Date(NA) else min(DS_DATE, na.rm = TRUE),
     .groups = "drop"
   )
 
 ds <- ds %>%
   left_join(ref_dates, by = "USUBJID") %>%
   mutate(
-    DSSTDY = ifelse(
-      is.na(REF_DATE),
-      NA_integer_,
-      as.integer(DS_DATE - REF_DATE + 1)
-    )
-  )
-
-ds <- ds %>%
-  arrange(USUBJID, DSDTC) %>%
+    DSSTDY = ifelse(is.na(REF_DATE), NA_integer_, as.integer(DS_DATE - REF_DATE + 1L))
+  ) %>%
+  arrange(STUDYID, USUBJID, DSDTC, DSTERM) %>%
   group_by(USUBJID) %>%
   mutate(DSSEQ = row_number()) %>%
   ungroup()
@@ -120,14 +123,15 @@ ds_final <- ds %>%
     DSTERM,
     DSDECOD,
     DSCAT,
-    VISITNUM = INSTANCE,
-    VISIT    = INSTANCE,
+    VISITNUM,
+    VISIT,
     DSDTC,
     DSSTDTC,
     DSSTDY
-  )
+  ) %>%
+  arrange(STUDYID, USUBJID, DSSEQ)
 
 ds_final
 
-# print(ds[1:3,8:16])
-
+stopifnot(all(c("STUDYID","DOMAIN","USUBJID","DSSEQ","DSTERM","DSDECOD","DSCAT",
+                "VISITNUM","VISIT","DSDTC","DSSTDTC","DSSTDY") %in% names(ds_final)))
